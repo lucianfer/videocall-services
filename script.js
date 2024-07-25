2a99e2743aed1f99ae94fe00a2a6c9d450570fdc
lucianfer <kunrex6@gmail.com>
2024-07-25 21:57:12 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// script.js
+/ script.js
 document.getElementById('start-instagram-call').onclick = function() {
     window.open('https://www.instagram.com/queenj4ruhh?igsh=MXJqNjc0c2lqaXdnbw==', '_blank');
 };
